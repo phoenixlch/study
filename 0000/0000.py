45711c286ff3ae5492388e033f38c8c37aeff86d
Phoenix <phoenixlch@msn.com>
2016-03-09 13:33:15 +0800

    add 0000.py

---
+++ 0000/0000.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from PIL import Image, ImageDraw, ImageFont
+
+# number = str(5)
+# color = (255.0, 0)
+# windows_font = 'arial.ttf'
+
+
+# def draw_text(text, fill_color, windows_font):
+#     im = Image.open('mm.bmp')
+#     x, y = im.size
+#     draw = ImageDraw.Draw(im)
+#     font = ImageFont.truetype(windows_font, 35)
+#     draw.text((x - 20, 7), text, fill_color, font)
+
+#     im.save('bbb.bmp')
+# draw_text(number, color, windows_font)
