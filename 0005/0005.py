d33241f5e68691553430aaa27490ff1ba9184bec
Phoenix <phoenixlch@msn.com>
2016-04-08 10:51:16 +0800

    add 0004 skip 0003
    Signed-off-by: Phoenix <phoenixlch@msn.com>

--- 0005/0005.py
+++ 0005/0005.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python
 #coding=utf-8
+
+import os
+from PIL import Image
+
+iphone5_WIDTH=1136
+iphone5_HEIGHT=640
+
+def resize_iphone5_pic(path,new_path,width=iphone5_WIDTH,height=iphone5_HEIGHT):
+	im = Image.open(path)
+	w,h=im.size
+
+	if w>width:
+		h=width*h//w
+		w=width
+	if h>height:
+		w=height*w//h
+		h=height
+
+
---
+++ small/list_01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+namelist=[]
+print 'Please enter the five names:'
+for i in range(5):
+	name=raw_input()
+	namelist.append(name)
+print "The name is:",namelist
